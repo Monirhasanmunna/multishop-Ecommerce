4cb323764b8cbb9bf76693a80f8cdc1fc41e77ae
munna <monirhasanmunna1@gmail.com>
2022-09-02 17:03:52 +0600

    admin panel layout create route and controller create

---
+++ resources/views/layouts/partial/backend/sidebar.blade.php
@@ -0,0 +1,37 @@
+<!-- Sidebar -->
+<ul class="navbar-nav sidebar sidebar-light accordion" id="accordionSidebar">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+      <div class="sidebar-brand-icon">
+        <img src="{{asset('backend/img/logo/logo2.png')}}">
+      </div>
+      <div class="sidebar-brand-text mx-3">RuangAdmin</div>
+    </a>
+    <hr class="sidebar-divider my-0">
+    <li class="nav-item active">
+      <a class="nav-link" href="{{route('dashboard')}}">
+        <i class="fas fa-fw fa-tachometer-alt"></i>
+        <span>Dashboard</span></a>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="sidebar-heading">
+      Features
+    </div>
+    
+    <li class="nav-item">
+      <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseForm" aria-expanded="true"
+        aria-controls="collapseForm">
+        <i class="fab fa-fw fa-wpforms"></i>
+        <span>Forms</span>
+      </a>
+      <div id="collapseForm" class="collapse" aria-labelledby="headingForm" data-parent="#accordionSidebar">
+        <div class="bg-white py-2 collapse-inner rounded">
+          <h6 class="collapse-header">Forms</h6>
+          <a class="collapse-item" href="form_basics.html">Form Basics</a>
+          <a class="collapse-item" href="form_advanceds.html">Form Advanceds</a>
+        </div>
+      </div>
+    </li>
+    <hr class="sidebar-divider">
+    <div class="version" id="version-ruangadmin"></div>
+  </ul>
+  <!-- Sidebar -->
