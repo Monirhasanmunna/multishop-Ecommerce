dfd4f5d441c441eee1c6a026d999e980aa8d1838
munna <monirhasanmunna1@gmail.com>
2022-09-07 00:01:10 +0600

    login authorize define by admin

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\User;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -28,11 +29,17 @@ class AuthenticatedSessionController extends Controller
      */
     public function store(LoginRequest $request)
     {
-        $request->authenticate();
+        $user = User::where('email',$request->email)->first();
 
-        $request->session()->regenerate();
+        if($user->status == true){
 
-        return redirect()->intended(RouteServiceProvider::HOME);
+            $request->authenticate();
+            $request->session()->regenerate();
+            return redirect()->intended(RouteServiceProvider::HOME);
+
+        }else{
+            abort(403, 'Your Account Has Been Disable By Admin.');
+        }  
     }
 
     /**
